81ef214848c7861309e794151d6c37b6f8248374
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2023-04-27 17:06:50 +0200

    Fix formatting

--- Problems/Problem 1/solution.asm
+++ Problems/Problem 1/solution.asm
@@ -7,54 +7,54 @@
 ;  Find the sum of all the multiples of 3 or 5 below 1000.
 ;
 ; -------------------------------------------------------------------------------------------------
-		global	main
-		extern	printf
+		global		main
+		extern		printf
 
-		section .data
-		msg	db "%i", 0x0a, 0x00		
+		section		.data
+		msg		db "%i", 0x0a, 0x00		
 
 
-		section	.text
-main:		push 	ebp
-		mov	ebp, esp
+		section		.text
+main:		push		ebp
+		mov		ebp, esp
 		
-		xor 	edi, edi			; sum = 0
-		xor	esi, esi			; i = 0
+		xor		edi, edi		; sum = 0
+		xor		esi, esi		; i = 0
 
 		; calculate sum		
 loop:		
 		; check division by 3
-		mov	eax, esi
-		xor	edx, edx
-		mov	ebx, 3
-		div	ebx
-		cmp	edx, 0
-		je	divisible
+		mov		eax, esi
+		xor		edx, edx
+		mov		ebx, 3
+		div		ebx
+		cmp		edx, 0
+		je		divisible
 		
 		; check division by 5
-		mov	eax, esi
-		xor	edx, edx
-		mov	ebx, 5
-		div	ebx
-		cmp	edx, 0
-		je	divisible
+		mov		eax, esi
+		xor		edx, edx
+		mov		ebx, 5
+		div		ebx
+		cmp		edx, 0
+		je		divisible
 
 		; non-divisible
-		jmp	increment
+		jmp		increment
 
-divisible:	add	edi, esi
+divisible:	add		edi, esi
 
-increment:	inc	esi
-		cmp	esi, 1000
-		jne	loop				; if esi < 1000
+increment:	inc		esi
+		cmp		esi, 1000
+		jne		loop			; if esi < 1000
 
 		; printing
-		push	edi
-		push	msg
-		call	printf
+		push		edi
+		push		msg
+		call		printf
 
 		; handle stack stuff
-		mov	eax, 0			; return code
-		mov	esp, ebp
-		pop 	ebp
+		mov		eax, 0			; return code
+		mov		esp, ebp
+		pop 		ebp
 		ret	
